ab9ea632e18f3e2e03a03ddf61733c7b50a74f73
ADBond <andy@themindlab.co.uk>
2020-10-14 23:27:36 +0100

    get relevant files served up, so can dev locally

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Attempting to motivate fitness with the carrot of data.
 
 Deployed page lives [here](https://adbond.github.io/run-stats/).
+
+## using
+
+There is a very basic server set up to serve csv file, stolen from Skat page.
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import (
-    Flask, render_template, send_from_directory
+    Flask, send_file, send_from_directory
 )
 
 
@@ -12,13 +12,24 @@ def make_app():
     print("fetching the d3 page...")
     return "basic server"
 
+  @app.route("/live")
+  def live_page_version():
+    return send_file("index.html")
+
   @app.route("/runs.csv")
   def get_data():
-    print("Getting ratings data")
     return send_from_directory(
       "./",
       "runs.csv",
       mimetype='text/csv',
     )
+
+  # TODO: should probably put stuff in folders etc
+  @app.route("/summarise.js")
+  def js():
+    return send_file("summarise.js")
+  @app.route("/style.css")
+  def style_sheet():
+    return send_file("style.css")
 
   return app

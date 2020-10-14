1ccd73ba230715cbb81a7e9701b50c8be82e5723
ADBond <andy@themindlab.co.uk>
2020-10-14 22:33:53 +0100

    attempt to get basic summary displayed

---
+++ summarise.js
@@ -0,0 +1,8 @@
+let csv = d3.csv(
+    "runs.csv",
+    function(data){
+        console.log(data);
+        let total_distance = data.forEach(run_datum => run_datum["distance_km"]).reduce((x, y) => x + y, 0);
+        d3.select("#total-distance").textContent = total_distance;
+    }
+);

e066da808f751145135fd283e360c8d6e0e35a3c
ADBond <andy@themindlab.co.uk>
2020-10-14 22:43:30 +0100

    syntax stuff

--- summarise.js
+++ summarise.js
@@ -1,8 +1,9 @@
 let csv = d3.csv(
     "runs.csv",
     function(data){
+        console.log("run data read in nicely:");
         console.log(data);
-        let total_distance = data.forEach(run_datum => run_datum["distance_km"]).reduce((x, y) => x + y, 0);
+        let total_distance = data.forEach(run_datum => run_datum.distance_km).reduce((x, y) => x + y, 0);
         d3.select("#total-distance").textContent = total_distance;
     }
 );

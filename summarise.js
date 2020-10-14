71fabf9cb6f4cca5ed485d25b63bbdda636ffb14
ADBond <andy@themindlab.co.uk>
2020-10-14 22:55:10 +0100

    I've forgotten how d3 works

--- summarise.js
+++ summarise.js
@@ -1,9 +1,12 @@
 let csv = d3.csv(
     "runs.csv",
-    function(data){
+    function(datum){
         console.log("run data read in nicely:");
-        console.log(data);
-        let total_distance = data.forEach(run_datum => run_datum.distance_km).reduce((x, y) => x + y, 0);
-        d3.select("#total-distance").textContent = total_distance;
+        console.log(datum);
+        return {
+            "distance_km": +datum.distance_km
+        };
     }
 );
+let total_distance = csv.forEach(run_datum => run_datum.distance_km).reduce((x, y) => x + y, 0);
+d3.select("#total-distance").textContent = total_distance;

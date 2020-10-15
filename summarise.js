ed5b82318acc3d06335a65fba34a993ce5b32ff0
ADBond <andy@themindlab.co.uk>
2020-10-15 19:24:12 +0100

    fetching out the best 5k time, with some tolerance

--- summarise.js
+++ summarise.js
@@ -22,6 +22,11 @@ function hmsStringFromSeconds(seconds){
     return `${hoursFromSeconds(seconds)} hours, ${leftoverMinutesFromSeconds(seconds)} minutes, ${leftoverSecondsFromSeconds(seconds)} seconds`
 }
 
+function isFiveK(distance_km){
+    const tolerance = 0.05;
+    return ((5 - tolerance) < distance_km) && (distance_km < (5 + tolerance));
+}
+
 d3.csv(
     "runs.csv",
     function(datum){
@@ -39,10 +44,24 @@ d3.csv(
 
         const distances = csv_data.map(run_datum => run_datum.distance_km);
         const times = csv_data.map(run_datum => run_datum.total_seconds);
-        
+
         const total_distance = distances.reduce((x, y) => x + y, 0);
         const total_seconds = times.reduce((x, y) => x + y, 0);
 
+        const five_ks = csv_data.filter(datum => isFiveK(datum.distance_km));
+        let best_five = {"total_seconds": Infinity};
+        five_ks.forEach(
+            datum => {
+                console.log("dataa")
+                console.log(datum);
+                console.log(best_five);
+                if(datum.total_seconds < best_five.total_seconds){
+                    best_five = datum;
+                }
+            }
+        );
+        console.log(five_ks);
+        console.log(best_five);
         d3.select("#total-distance").text(total_distance);
         d3.select("#total-distance-miles").text(kmToMiles(total_distance));
         d3.select("#total-time").text(hmsStringFromSeconds(total_seconds));

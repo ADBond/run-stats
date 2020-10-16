bb5a72fa0242f640eb8228e3f9fca74b129110d0
ADBond <andy@themindlab.co.uk>
2020-10-16 18:55:20 +0100

    v minor styling, some crude summary counts

--- summarise.js
+++ summarise.js
@@ -38,8 +38,6 @@ function isFiveK(distance_km){
 d3.csv(
     "runs.csv",
     function(datum){
-        console.log("run data read in nicely:");
-        console.log(datum);
         return {
             "distance_km": +datum.distance_km,
             "total_seconds": timeToSeconds(datum.hours, datum.minutes, datum.seconds)
@@ -47,8 +45,6 @@ d3.csv(
     }
 ).then(
     function(csv_data){
-        console.log("everything is:");
-        console.log(csv_data);
 
         const distances = csv_data.map(run_datum => run_datum.distance_km);
         const times = csv_data.map(run_datum => run_datum.total_seconds);
@@ -56,13 +52,14 @@ d3.csv(
         const total_distance = distances.reduce((x, y) => x + y, 0);
         const total_seconds = times.reduce((x, y) => x + y, 0);
 
+        // TODO: geting to the point where I need to start structuring this a bit more proper like
         const five_ks = csv_data.filter(datum => isFiveK(datum.distance_km));
+        const ten_ks = [];
+        const halfs = [];
+        const marathons = [];
         let best_five = {"total_seconds": Infinity};
         five_ks.forEach(
             datum => {
-                console.log("dataa")
-                console.log(datum);
-                console.log(best_five);
                 if(datum.total_seconds < best_five.total_seconds){
                     // best by total time currently only
                     // TODO: best by pace (which would account for variation in distance!)
@@ -70,12 +67,20 @@ d3.csv(
                 }
             }
         );
-        console.log(five_ks);
-        console.log(best_five);
+
         d3.select("#total-distance").text(total_distance);
         d3.select("#total-distance-miles").text(kmToMiles(total_distance));
         d3.select("#total-time").text(hmsStringFromSeconds(total_seconds));
         d3.select("#total-time-seconds").text(total_seconds);
+
         d3.select("#best-5k").text(hmsStringFromSeconds(best_five.total_seconds));
+
+        d3.select("#count-total").text(csv_data.length);
+        d3.select("#count-5k").text(five_ks.length);
+        d3.select("#count-10k").text(ten_ks.length);
+        d3.select("#count-half").text(halfs.length);
+        d3.select("#count-marathon").text(marathons.length);
+        // yeah yeah I know
+        d3.select("#count-misc").text(csv_data.length - five_ks.length - ten_ks.length - halfs.length - marathons.length);
     }
 );
